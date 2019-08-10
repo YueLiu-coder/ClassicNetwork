b8e0d8ae0fe1a0d4b564b44ea828140d0d8f60fd
shanglianlm0525 <569793357@qq.com>
2019-08-11 02:45:46 +0800

    commit InceptionV3

--- InceptionV1.py
+++ InceptionV1.py
@@ -2,26 +2,27 @@ import torch
 import torch.nn as nn
 import torchvision
 
-def ConvBN(in_channels,out_channels,kernel_size):
+def ConvBNReLU(in_channels,out_channels,kernel_size):
     return nn.Sequential(
         nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=1,padding=kernel_size//2),
-        nn.BatchNorm2d(out_channels)
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU6(inplace=True)
     )
 
 class InceptionV1Module(nn.Module):
     def __init__(self, in_channels,out_channels1, out_channels2reduce,out_channels2, out_channels3reduce, out_channels3, out_channels4):
         super(InceptionV1Module, self).__init__()
 
-        self.branch1_conv = ConvBN(in_channels=in_channels,out_channels=out_channels1,kernel_size=1)
+        self.branch1_conv = ConvBNReLU(in_channels=in_channels,out_channels=out_channels1,kernel_size=1)
 
-        self.branch2_conv1 = ConvBN(in_channels=in_channels,out_channels=out_channels2reduce,kernel_size=1)
-        self.branch2_conv2 = ConvBN(in_channels=out_channels2reduce,out_channels=out_channels2,kernel_size=3)
+        self.branch2_conv1 = ConvBNReLU(in_channels=in_channels,out_channels=out_channels2reduce,kernel_size=1)
+        self.branch2_conv2 = ConvBNReLU(in_channels=out_channels2reduce,out_channels=out_channels2,kernel_size=3)
 
-        self.branch3_conv1 = ConvBN(in_channels=in_channels, out_channels=out_channels3reduce, kernel_size=1)
-        self.branch3_conv2 = ConvBN(in_channels=out_channels3reduce, out_channels=out_channels3, kernel_size=5)
+        self.branch3_conv1 = ConvBNReLU(in_channels=in_channels, out_channels=out_channels3reduce, kernel_size=1)
+        self.branch3_conv2 = ConvBNReLU(in_channels=out_channels3reduce, out_channels=out_channels3, kernel_size=5)
 
         self.branch4_pool = nn.MaxPool2d(kernel_size=3,stride=1,padding=1)
-        self.branch4_conv1 = ConvBN(in_channels=in_channels, out_channels=out_channels4, kernel_size=1)
+        self.branch4_conv1 = ConvBNReLU(in_channels=in_channels, out_channels=out_channels4, kernel_size=1)
 
     def forward(self,x):
         out1 = self.branch1_conv(x)
@@ -31,9 +32,28 @@ class InceptionV1Module(nn.Module):
         out = torch.cat([out1, out2, out3, out4], dim=1)
         return out
 
+class InceptionAux(nn.Module):
+    def __init__(self, in_channels,out_channels):
+        super(InceptionAux, self).__init__()
+
+        self.auxiliary_avgpool = nn.AvgPool2d(kernel_size=5, stride=3)
+        self.auxiliary_conv1 = ConvBNReLU(in_channels=in_channels, out_channels=128, kernel_size=1)
+        self.auxiliary_linear1 = nn.Linear(in_features=128 * 4 * 4, out_features=1024)
+        self.auxiliary_relu = nn.ReLU6(inplace=True)
+        self.auxiliary_dropout = nn.Dropout(p=0.7)
+        self.auxiliary_linear2 = nn.Linear(in_features=1024, out_features=out_channels)
+
+    def forward(self, x):
+        x = self.auxiliary_conv1(self.auxiliary_avgpool(x))
+        x = x.view(x.size(0), -1)
+        x= self.auxiliary_relu(self.auxiliary_linear1(x))
+        out = self.auxiliary_linear2(self.auxiliary_dropout(x))
+        return out
+
 class InceptionV1(nn.Module):
-    def __init__(self, num_class=1000):
+    def __init__(self, num_classes=1000, stage='train'):
         super(InceptionV1, self).__init__()
+        self.stage = stage
 
         self.block1 = nn.Sequential(
             nn.Conv2d(in_channels=3,out_channels=64,kernel_size=7,stride=2,padding=3),
@@ -56,12 +76,8 @@ class InceptionV1(nn.Module):
 
         self.block4_1 = InceptionV1Module(in_channels=480, out_channels1=192, out_channels2reduce=96, out_channels2=208,out_channels3reduce=16, out_channels3=48, out_channels4=64)
 
-        self.auxiliary1_avgpool = nn.AvgPool2d(kernel_size=5, stride=3)
-        self.auxiliary1_conv1 = ConvBN(in_channels=512, out_channels=128, kernel_size=1)
-        self.auxiliary1_linear1 = nn.Linear(in_features=128*4*4, out_features=1024)
-        self.auxiliary1_relu = nn.ReLU6(inplace=True)
-        self.auxiliary1_dropout = nn.Dropout(p=0.7)
-        self.auxiliary1_linear2 = nn.Linear(in_features=1024, out_features=num_class)
+        if self.stage == 'train':
+            self.aux_logits1 = InceptionAux(in_channels=512,out_channels=num_classes)
 
         self.block4_2 = nn.Sequential(
             InceptionV1Module(in_channels=512, out_channels1=160, out_channels2reduce=112, out_channels2=224,
@@ -72,12 +88,8 @@ class InceptionV1(nn.Module):
                               out_channels3reduce=32, out_channels3=64, out_channels4=64),
         )
 
-        self.auxiliary2_avgpool = nn.AvgPool2d(kernel_size=5, stride=3)
-        self.auxiliary2_conv1 = ConvBN(in_channels=528, out_channels=128, kernel_size=1)
-        self.auxiliary2_linear1 = nn.Linear(in_features=128*4*4, out_features=1024)
-        self.auxiliary2_relu = nn.ReLU6(inplace=True)
-        self.auxiliary2_dropout = nn.Dropout(p=0.7)
-        self.auxiliary2_linear2 = nn.Linear(in_features=1024, out_features=num_class)
+        if self.stage == 'train':
+            self.aux_logits2 = InceptionAux(in_channels=528,out_channels=num_classes)
 
         self.block4_3 = nn.Sequential(
             InceptionV1Module(in_channels=528, out_channels1=256, out_channels2reduce=160, out_channels2=320,
@@ -92,36 +104,33 @@ class InceptionV1(nn.Module):
 
         self.avgpool = nn.AvgPool2d(kernel_size=7,stride=1)
         self.dropout = nn.Dropout(p=0.4)
-        self.linear = nn.Linear(in_features=1024,out_features=num_class)
+        self.linear = nn.Linear(in_features=1024,out_features=num_classes)
 
     def forward(self, x):
         x = self.block1(x)
         x = self.block2(x)
         x = self.block3(x)
-        out1 = x = self.block4_1(x)
-        out1 = self.auxiliary1_conv1(self.auxiliary1_avgpool(out1))
-        out1 = out1.view(out1.size(0), -1)
-        out1 = self.auxiliary1_linear2(self.auxiliary1_dropout(self.auxiliary1_relu(self.auxiliary1_linear1(out1))))
-
-        out2 = x = self.block4_2(x)
-        out2 = self.auxiliary2_conv1(self.auxiliary2_avgpool(out2))
-        out2 = out2.view(out2.size(0), -1)
-        out2 = self.auxiliary2_linear2(self.auxiliary2_dropout(self.auxiliary2_relu(self.auxiliary2_linear1(out2))))
-
+        aux1 = x = self.block4_1(x)
+        aux2 = x = self.block4_2(x)
         x = self.block4_3(x)
-        out3 = self.block5(x)
-        out3 = self.avgpool(out3)
-        out3 = self.dropout(out3)
-        out3 = out3.view(out3.size(0), -1)
-        out3 = self.linear(out3)
-        return out1, out2, out3
+        out = self.block5(x)
+        out = self.avgpool(out)
+        out = self.dropout(out)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        if self.stage == 'train':
+            aux1 = self.aux_logits1(aux1)
+            aux2 = self.aux_logits2(aux2)
+            return aux1, aux2, out
+        else:
+            return out
 
 if __name__=='__main__':
     model = InceptionV1()
     print(model)
 
     input = torch.randn(1, 3, 224, 224)
-    out1,out2,out3 = model(input)
-    print(out1.shape)
-    print(out2.shape)
-    print(out3.shape)
+    aux1, aux2, out = model(input)
+    print(aux1.shape)
+    print(aux2.shape)
+    print(out.shape)
